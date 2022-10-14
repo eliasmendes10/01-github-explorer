6dc927d7d71789833949421a9e8155aab6250166
Elias <eliasmdes@gmail.com>
2022-10-14 07:50:36 -0300

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
 export function App() {
+  throw new Error("Internal server error");
   return <h1>Hello ReactJS</h1>;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+const isDevelopment = process.env.NODE_ENV !== "production";
+
 module.exports = {
-  mode: "development",
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -12,7 +15,6 @@ module.exports = {
     extensions: [".js", ".jsx"],
   },
   devServer: {
-    // contentBase
     static: {
       directory: path.join(__dirname, "public"),
     },
