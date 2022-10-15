b6676d84b82598f6d8e8a7a72a7fee974a9aa768
Elias <eliasmdes@gmail.com>
2022-10-15 08:25:26 -0300

    finalizada etapa conceitos importantes

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button type="button" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
