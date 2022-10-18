4ef0c883c8ca9e97572d74b835ad77c37aee16a4
Elias <eliasmdes@gmail.com>
2022-10-17 22:18:03 -0300

    useState e useEffect

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,11 +1,10 @@
 export function RepositoryItem(props) {
-  console.log(props);
   return (
     <li>
       <strong>{props.repository.name ?? "Default"}</strong>
       <p>{props.repository.description}</p>
 
-      <a href={props.repository.link}>Acessar Repositório</a>
+      <a href={props.repository.html_url}>Acessar Repositório</a>
     </li>
   );
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,30 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
-
-const repository = {
-  name: "unform2",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform",
-};
+import "../styles/repositories.scss";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  //0 - Se uma variável mudar a função é acionada
+  //1 - se o segundo parametro for um array vazio [] ela irá executar uma única vez
+  //2 - Nunca deixar sem o segundo parametro, pq o useEffect vai entrar em loop
+  //3 - nunca alterar a o valor da variável que é o target também com a conseguencia de loop
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repository) => {
+          return (
+            <RepositoryItem key={repository.name} repository={repository} />
+          );
+        })}
       </ul>
     </section>
   );
